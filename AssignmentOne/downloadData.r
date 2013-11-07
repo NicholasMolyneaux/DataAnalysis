4a2a94b9c81e3a49b48d172ff1340d769f6a9800
Nicholas Molyneaux <nicholas.molyneaux@epfl.ch>
2013-11-07 23:05:45 +0100

    Downloaded data

---
+++ AssignmentOne/downloadData.r
@@ -0,0 +1,4 @@
+fileURLrda="https://spark-public.s3.amazonaws.com/dataanalysis/loansData.rda"
+fileURLcsv="https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv"
+download.file(fileURLrda,"loansData.rda")
+download.file(fileURLcsv,"loansData.csv")
